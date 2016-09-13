600ebde6f904a36737fbf58df40872ec97994df5
borsch <oleh.kurpiak@gmail.com>
2016-09-13 20:19:48 +0300

    init arcitecture

---
+++ client/src/main/java/checkers/client/Main.java
@@ -0,0 +1,12 @@
+package checkers.client;
+
+/**
+ * Created by oleh_kurpiak on 13.09.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("client");
+    }
+
+}
